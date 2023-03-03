ccaccc5fca7711ca8e11fb7b095b08aba5c84fe6
aldidaim21 <aldidaimfauzan21@gmail.com>
2023-03-03 21:15:26 +0700

    anjay mabar

---
+++ latihan oop/class/pendahuluan.py
@@ -0,0 +1,19 @@
+class hero:  # tamplate
+    pass
+
+
+hero1 = hero()
+hero2 = hero()
+
+hero1.name = "sniper"
+hero1.health = 100
+
+
+print(hero1)
+print(hero1.__dict__)
+
+
+# # tidak menggunakan class
+# heroName = "ucup"
+# sniperattack = "100"
+# herohealth = "1000"
